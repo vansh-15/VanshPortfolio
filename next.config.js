7604ffc5f1bf48a4a0c6e1d49f19a2f7d196cb34
Vansh Mishra <164024648+vansh-15@users.noreply.github.com>
2026-08-25 23:21:07 +0530

    deplouable

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
     basePath: '/VanshPortfolio',
     assetPrefix: '/VanshPortfolio/',
-    images: {
-        unoptimized: true,
-    },
 };
 
 module.exports = nextConfig;
